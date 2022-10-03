1af05ab7a641ce8addec90f94047381c600b5852
Nicolás Gómez <ngomez@hey.com>
2022-10-03 19:29:29 -0300

    add item

--- src/Expense.js
+++ src/Expense.js
@@ -35,68 +35,88 @@ export default function Expense() {
   // - Poder agregar un nuevo item
   // - Eliminar un item
 
+  // TODO: revisar si es buena idea ocupar
+
   console.log(items);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>$</th>
-          {people.map(name => <th key={name}>{name}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item, itemIndex) => (
+    <>
+      <table>
+        <thead>
           <tr>
-            <td>
-              <input
-                type="text"
-                placeholder="Nombre de item"
-                value={item.name}
-                onChange={(event) => {
-                  const newState = [...items];
-                  newState[itemIndex] = {
-                    ...item,
-                    name: event.target.value,
-                  }
-                  setItems(newState);
-                }}
-              />
-            </td>
-            <td>
-              <input
-                type="number"
-                placeholder="precio"
-                value={item.price}
-                onChange={(event) => {
-                  const newState = [...items];
-                  newState[itemIndex] = {
-                    ...item,
-                    price: parseInt(event.target.value),
-                  }
-                  setItems(newState);
-                }}
-              />
-            </td>
-            {item.distribution.map((count, distributionIndex) => (
+            <th>Item</th>
+            <th>$</th>
+            {people.map(name => <th key={name}>{name}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, itemIndex) => (
+            <tr>
               <td>
                 <input
-                  type="number"
-                  value={count}
-                  min={0}
+                  type="text"
+                  placeholder="Nombre de item"
+                  value={item.name}
                   onChange={(event) => {
-                    const newValue = parseInt(event.target.value);
-                    const newState = [...items]; // <--- Revisar esto
-                    newState[itemIndex].distribution[distributionIndex] = newValue;
+                    const newState = [...items];
+                    newState[itemIndex] = {
+                      ...item,
+                      name: event.target.value,
+                    }
                     setItems(newState);
                   }}
                 />
               </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              <td>
+                <input
+                  type="number"
+                  placeholder="precio"
+                  value={item.price}
+                  onChange={(event) => {
+                    const newState = [...items];
+                    newState[itemIndex] = {
+                      ...item,
+                      price: parseInt(event.target.value),
+                    }
+                    setItems(newState);
+                  }}
+                />
+              </td>
+              {item.distribution.map((count, distributionIndex) => (
+                <td>
+                  <input
+                    type="number"
+                    value={count}
+                    min={0}
+                    onChange={(event) => {
+                      const newValue = parseInt(event.target.value);
+                      const newState = [...items]; // <--- Revisar esto
+                      newState[itemIndex].distribution[distributionIndex] = newValue;
+                      setItems(newState);
+                    }}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{ textAlign: 'left' }}>
+        <button
+          onClick={() => {
+            setItems([
+              ...items,
+              {
+                name: null,
+                price: null,
+                distribution: people.map(() => 0)
+              }
+            ])
+          }}
+        >
+          Agregar item
+        </button>
+      </div>
+    </>
   )
 }
