71fa3cdaf57c9cc9e0149cb4e46e261962369512
Nicolás Gómez <ngomez@hey.com>
2022-10-26 16:33:09 -0300

    feat: add bootstrap

--- src/Hero.js
+++ src/Hero.js
@@ -1,10 +1,10 @@
 export default function Hero({ onClick }) {
   return (
     <div>
-      <h1>Salidas 🍔</h1>
+      <h1 className="fw-bold mt-3">Salidas 🍔</h1>
       <p>¿Saliste y pagaste tu la cuenta? 😱</p>
       <p>No te preocupes. Divide la cuenta de manera fácil.</p>
-      <button onClick={onClick}>Nuevo gasto</button>
+      <button className="btn btn-primary" onClick={onClick}>Nuevo gasto</button>
     </div>
   );
 }
